bccf87f7f8aca404339a0dd053d12904fa79edc9
Preetham Gujjula <gitcommit@mail.preetham.io>
2021-12-28 20:00:09 -0600

    add draft version of primes

--- src/Math/NumberTheory/PrimeSieve.hs
+++ src/Math/NumberTheory/PrimeSieve.hs
@@ -8,6 +8,25 @@
 -- This module provides a high-level, polymorphic interface to the primesieve
 -- library. For a lower-level interface, see "Math.NumberTheory.PrimeSieve.FFI#".
 module Math.NumberTheory.PrimeSieve
-  ( 
+  ( primes,
   )
 where
+
+import Data.Word (Word64)
+import Foreign.Marshal.Alloc (mallocBytes)
+import Math.NumberTheory.PrimeSieve.FFI
+  ( primesieveIteratorSize,
+    primesieve_init,
+    primesieve_next_prime
+  )
+import System.IO.Unsafe (unsafeInterleaveIO, unsafePerformIO)
+
+iterateIO :: IO a -> IO [a]
+iterateIO mx = (:) <$> mx <*> unsafeInterleaveIO (iterateIO mx)
+
+primes :: [Word64]
+primes = unsafePerformIO $ do
+  iter <- mallocBytes primesieveIteratorSize
+  primesieve_init iter
+  iterateIO (primesieve_next_prime iter)
+{-# NOINLINE primes #-}
--- src/Math/NumberTheory/PrimeSieve/FFI.hs
+++ src/Math/NumberTheory/PrimeSieve/FFI.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CApiFFI #-}
+
 -- |
 -- Module      : Math.NumberTheory.PrimeSieve
 -- Copyright   : 2021 Preetham Gujjula
@@ -13,6 +15,73 @@
 -- (https://github.com/kimwalisch/primesieve/blob/master/doc/libprimesieve.md#c-api-reference)
 -- and [@primesieve.h@](https://github.com/kimwalisch/primesieve/blob/master/include/primesieve.h).
 module Math.NumberTheory.PrimeSieve.FFI
-  ( 
+  ( primesieve_count_primes,
+    primesieve_count_twins,
+    primesieve_count_triplets,
+    primesieve_count_quadruplets,
+    primesieve_count_quintuplets,
+    primesieve_count_sextuplets,
+    primesieve_print_primes,
+    -- iterator
+    PrimesieveIterator,
+    primesieveIteratorSize,
+    primesieve_init,
+    primesieve_free_iterator,
+    primesieve_skipto,
+    primesieve_next_prime,
+    primesieve_prev_prime,
   )
 where
+
+import Data.Int (Int64)
+import Data.Word (Word64)
+import Foreign.Ptr (Ptr)
+
+--
+-- From primesieve.h
+--
+
+foreign import ccall unsafe "primesieve_count_primes"
+  primesieve_count_primes :: Int64 -> Int64 -> Int64
+
+foreign import ccall unsafe "primesieve_count_twins"
+  primesieve_count_twins :: Int64 -> Int64 -> Int64
+
+foreign import ccall unsafe "primesieve_count_triplets"
+  primesieve_count_triplets :: Int64 -> Int64 -> Int64
+
+foreign import ccall unsafe "primesieve_count_quadruplets"
+  primesieve_count_quadruplets :: Int64 -> Int64 -> Int64
+
+foreign import ccall unsafe "primesieve_count_quintuplets"
+  primesieve_count_quintuplets :: Int64 -> Int64 -> Int64
+
+foreign import ccall unsafe "primesieve_count_sextuplets"
+  primesieve_count_sextuplets :: Int64 -> Int64 -> Int64
+
+foreign import ccall unsafe "primesieve_print_primes"
+  primesieve_print_primes :: Int64 -> Int64 -> IO ()
+
+--
+-- From primesieve/iterator.h
+--
+
+data PrimesieveIterator
+
+primesieveIteratorSize :: Int
+primesieveIteratorSize = 80
+
+foreign import ccall unsafe "primesieve_init"
+  primesieve_init :: Ptr PrimesieveIterator -> IO ()
+
+foreign import ccall unsafe "primesieve_free_iterator"
+  primesieve_free_iterator :: Ptr PrimesieveIterator -> IO ()
+
+foreign import ccall unsafe "primesieve_skipto"
+  primesieve_skipto :: Ptr PrimesieveIterator -> Word64 -> Word64 -> IO ()
+
+foreign import capi "primesieve/iterator.h primesieve_next_prime"
+  primesieve_next_prime :: Ptr PrimesieveIterator -> IO Word64
+
+foreign import capi "primesieve/iterator.h primesieve_prev_prime"
+  primesieve_prev_prime :: Ptr PrimesieveIterator -> IO Word64
